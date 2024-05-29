098ac95adab649333df4c4b80f485674195a963f
JesusLopez28 <jesus.lopez280402@gmail.com>
2024-05-29 08:24:40 -0600

    25.49.132.248

--- src/MultihiloCliente.java
+++ src/MultihiloCliente.java
@@ -6,7 +6,7 @@ public class MultihiloCliente {
     static Random random = new Random();
     static final int id = random.nextInt(1000);
     static final String NOMBRE = "Calculadora " + id;
-    public static final String HOST = "localhost";
+    public static final String HOST = "25.49.132.248";
 
     public static void main(String[] args) throws IIOException {
         Thread cliente = new Thread(new Cliente(HOST, id, NOMBRE));
